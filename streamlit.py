b1995a211aceb99c25adf37d4db60d45480cb735
Bodhian1999 <126016003+Bodhian1999@users.noreply.github.com>
2023-03-23 17:05:29 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -112,8 +112,8 @@ st.code("laadpaaldata = laadpaaldata[laadpaaldata['ChargeTime'] >= 0]")
 st.header('RDW-data')
 
 st.markdown('**RDW data inladen met behulp van de API. Limit op 200.000 auto.**')
-st.code('response2 = requests.get("https://opendata.rdw.nl/resource/qyrd-w56j.json?$limit=200000&$offset=0")')
-response2 = requests.get("https://opendata.rdw.nl/resource/qyrd-w56j.json?$limit=200000&$offset=0")
+st.code('response2 = requests.get("https://opendata.rdw.nl/resource/qyrd-w56j.json?$limit=2000&$offset=0")')
+response2 = requests.get("https://opendata.rdw.nl/resource/qyrd-w56j.json?$limit=2000&$offset=0")
 
 st.markdown('**Omzetten naar json.**')
 st.code('responsejson2  = response2.json()')
